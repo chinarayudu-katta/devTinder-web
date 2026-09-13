e7ee2c40c197fa715b27cc12a051c71e0c1c66a3
unknown <chinarayudukatta@gmail.com>
2026-09-14 04:49:06 +0530

    add ingnored/interested request

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -23,7 +23,7 @@ const Connections = () => {
   }, [])
 
     if(!connections) return;
-    if(connections.length === 0) return <h1>No Connection Found</h1>
+    if(connections.length === 0) return <h1 className="flex justify-center my-10">No Connection Found</h1>
   return (
     <div className="text-center my-10">
       <h2 className="text-bold text-white text-2xl">Connections</h2>
